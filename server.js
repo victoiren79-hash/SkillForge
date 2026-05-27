ef0ad195fa1c4f7661ab802c5cae1ab562487ddf
NADJITIESSEM GONDJE Victoire <victoiren79@gmail.com>
2026-05-27 17:36:53 +0200

    Add Content-Security-Policy for Pendo script

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+// FIX: Tell browsers they are allowed to load the Pendo script from cdn.pendo.io
+app.use((req, res, next) => {
+    res.setHeader(
+        'Content-Security-Policy',
+        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.pendo.io; connect-src 'self' https://api.groq.com https://cdn.pendo.io https://app.pendo.io; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:;"
+    );
+    next();
+});
+
+// Serve the main HTML file
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // The secure API proxy
